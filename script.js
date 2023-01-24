b434513c017a34a066a59fd182735bb733ac394a
Sourav Dey <102379824+Srv228@users.noreply.github.com>
2023-01-24 13:34:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
+
+
 function letterCombinations(input_digit) {
   //Complete the function
+
+	 if(input_digit.length == 0) {
+	let bres = [];
+	bres.push("");
+	return bres;
+  }
+
+	
+	let ch = input_digit.slice(0,1); 
+	let ros = input_digit.slice(1); 
+
+	let rres = letterCombinations(ros);
+	let mres = [];
+
+	for(let i = 0; i < keys[ch].length; i++) {
+		let char = keys[ch][i];
+		for(rstr of rres) {
+			mres.push(char + rstr);
+		}
+	}
+
+	return mres;
+	
 }
 
 module.exports = letterCombinations;
