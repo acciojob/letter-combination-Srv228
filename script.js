7bbf365e003e8b87c541d3a5a22cadd9e1d4ae76
Sourav Dey <102379824+Srv228@users.noreply.github.com>
2023-01-24 13:37:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,44 @@
-const keys = [
-	"0",
-	"1",
-	"abc",
-	"def",
-	"ghi",
-	"jkl",
-	"mno",
-	"pqrs",
-	"tuv",
-	"wxyz",
-]
-
-function letterCombinations(input_digit) {
-  //Complete the function
-
-	 if(input_digit.length == 0) {
-	let bres = [];
-	bres.push("");
-	return bres;
-  }
-
-	
-	let ch = input_digit.slice(0,1); 
-	let ros = input_digit.slice(1); 
-
-	let rres = letterCombinations(ros);
-	let mres = [];
-
-	for(let i = 0; i < keys[ch].length; i++) {
-		let char = keys[ch][i];
-		for(rstr of rres) {
-			mres.push(char + rstr);
-		}
-	}
-
-	return mres;
-	
+function letterCombinations(digit) {
+	//   let arr = {
+	// 	  0 : 0,
+	// 	  1 : 1,
+	// 	  2 : {a,b,c},
+	// 	  3 : {d,e,f},
+	// 	  4 : {g,h,i},
+	// 	  5 : {j,k,l},
+	// 	  6 : {m,n,o},
+	// 	  7 : {p,q,r,s},
+	// 	  8 : {t,u,v},
+	// 	  9 : {w,x,y,z}
+	//   }
+ //  let n = input_digit.length;
+	// for(let i=0;i<n;i++){
+		
+	// }
+	   if (!digits) {
+        return [];
+    }
+    let map = {
+        2: "abc",
+        3: "def",
+        4: "ghi",
+        5: "jkl",
+        6: "mno",
+        7: "pqrs",
+        8: "tuv",
+        9: "wxyz"
+    };
+    let res = [""];
+    for (let i = 0; i < digits.length; i++) {
+        let tmp = [];
+        for (let j = 0; j < res.length; j++) {
+            for (let k = 0; k < map[digits[i]].length; k++) {
+                tmp.push(res[j] + map[digits[i]][k]);
+            }
+        }
+        res = tmp;
+    }
+    return res.sort();
 }
 
 module.exports = letterCombinations;
